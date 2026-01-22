0e1e44942c89cada3db523103414b68f23ef231a
chaitanya harish <chaitanyaharish080@gmail.com>
2026-01-22 11:26:41 +0530

    Store Procedure Added

---
+++ SPToGetEmpBySalaryAndDept.sql
@@ -0,0 +1,17 @@
+-- =============================================
+-- Author:		Chaitanya Harish
+-- Create date: 22-01-2026
+-- Description:	SampleData
+-- =============================================
+ALTER PROCEDURE Sp_GetemployesDetailsBySalaryAndDepartment 
+	-- Add the parameters for the stored procedure here
+	@department varchar(50),
+	@salary decimal (9,2)
+	
+AS
+BEGIN
+	SELECT * FROM Employee
+	WHERE Department = @department AND Salary <= @salary;
+END
+GO
+EXEC Sp_GetemployesDetailsBySalaryAndDepartment 'IT', 70000
