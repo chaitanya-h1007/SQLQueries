51e41d9d323b5e3e66c0ee4f55f5c7a24d40d8ba
chaitanya harish <chaitanyaharish080@gmail.com>
2026-01-22 14:46:26 +0530

    Update Crud Operation Using SP

--- CRUDOperations.sql
+++ CRUDOperations.sql
@@ -1,4 +1,15 @@
 ---CRUD OPERATION's UPDATE:
+
+-- NOTE :  DO NOT Update Primary Key Values in a Table.
+
+--UPDATE Employee
+--SET Department = 'HR', Salary = 60000, EmployeeID = 10
+--WHERE (EmployeeID = 17);  
+
+-- This will lead to data inconsistsy and to hurt the acid Principles in DBMS.
+
+
+GO
 UPDATE Employee
 SET Department = 'HR', Salary = 60000
 WHERE (EmployeeID = 10);
---
+++ UpdateSP.sql
@@ -0,0 +1,29 @@
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE Update_EmployeeSalaryAndDepartment
+	-- Add the parameters for the stored procedure here
+	@Id int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE Employee 
+	SET Department = 'HR' , Salary = 65000
+	WHERE EmployeeID = @Id;
+
+	SELECT * FROM Employee;
+END
+GO
+
+
+Exec Update_EmployeeSalaryAndDepartment 14
+
+
+
